4f7966bb4d38bd0f6005b31487ce631ec467e98d
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2022-03-09 01:53:36 -0500

    built counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {useState} from "react";
+
 function App() {
+
+  const [count, setCount] = useState(0)
+  
+  const increaseCount = () => {
+    setCount(count + 1)
+    console.log(count)
+  }
+
   return (
     <div className="App">
-      <p>Hello</p>
+      <p className="title">7 GUIs Challenge</p>
+      <div>
+        <p className="subtitle">Counter</p>
+        <input type="text" readOnly={true} value={count}/>
+        <button onClick={increaseCount}>Count</button>
+      </div>
     </div>
   );
 }
